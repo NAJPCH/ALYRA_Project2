579b05a0d981758f4f274c9bc622f95516e5a5dc
Nicolas Peytavin <nicolas.peytavin@protonmail.com>
2023-02-27 19:50:43 +0100

    Update 01_script.js

--- migrations/01_script.js
+++ migrations/01_script.js
@@ -1,12 +1,3 @@
-
-/*const Voting = artifacts.require("Voting");
-
-module.exports = async (deployer) => {
-  await deployer.deploy(Voting, 5, {value:100000000});
-  let instance = await Voting.deployed();
-  //console.log(await instance.retrieve());
-};*/
-
 const Voting = artifacts.require("Voting");
 
 module.exports = function (deployer) {
